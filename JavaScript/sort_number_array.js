9d91f035ce6822ad6670ff8a6d0b2be023c6b37d
Varg-Studios <jeremyfaith.me@gmail.com>
2015-02-22 23:23:01 -0500

    add example for function sortNumArray

--- JavaScript/sort_number_array.js
+++ JavaScript/sort_number_array.js
@@ -1,3 +1,11 @@
-array = array.sort(function(a,b) {
-    return a-b;
-  });
+function sortNumArray(arr) {
+
+	arr = arr.sort(function(a,b) {
+	    return a-b;
+	  });
+
+	return arr;
+
+}
+
+// sort([2,4,100,1,8,10,5]) returns [1,2,4,5,8,10,100]
